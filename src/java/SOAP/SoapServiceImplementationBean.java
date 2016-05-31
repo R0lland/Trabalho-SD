9006acfff47195818bcd73cd02e9eac15784c4b2
RDPzero <ricardo_rdpzero@hotmail.com>
2016-05-31 18:31:08 -0300

    Correções no SOAP

--- src/java/SOAP/SoapServiceImplementationBean.java
+++ src/java/SOAP/SoapServiceImplementationBean.java
@@ -10,39 +10,36 @@ import BD.OperacoesBD;
 import Entidades.Carro;
 import java.sql.SQLException;
 import java.util.List;
-import javax.jws.WebMethod;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
 
 /**
  *
  * @author Ricardo Deitoz Posser
  */
-@WebService
-@SOAPBinding(style = SOAPBinding.Style.RPC)
-public class SoapServiceImplementationBean {
+@WebService(endpointInterface="SOAP.SoapServiceEndpointInterface")
+public class SoapServiceImplementationBean implements SoapServiceEndpointInterface {
 
-    @WebMethod
+    @Override
     public void Adiciona(Carro carro) throws SQLException {
         OperacoesBD.adicionaCarro(carro);
     }
 
-    @WebMethod
+    @Override
     public void Altera(Carro carro) throws SQLException {
         OperacoesBD.alteraCarro(carro);
     }
 
-    @WebMethod
+    @Override
     public void Excluir(Carro carro) throws SQLException {
         OperacoesBD.deletaCarro(carro);
     }
 
-    @WebMethod
+    @Override
     public Carro Consulta(Integer codigo) throws SQLException {
         return OperacoesBD.consultaCarro(codigo);
     }
 
-    @WebMethod
+    @Override
     public Carro[] listaAnoModelo(Integer ano, String modelo) throws SQLException {
         List<Carro> lista = OperacoesBD.listaAnoModelo(ano, modelo);
         Carro[] array = new Carro[lista.size()];
