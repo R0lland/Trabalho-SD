72f5aeecf35f9c2ffa652d4b56011538843d42fc
Cristiano <cristiano.gehring@gmail.com>
2016-06-05 19:57:56 -0300

    removendo código desnecessário

--- src/java/UDP/EnviaFila.java
+++ src/java/UDP/EnviaFila.java
@@ -50,31 +50,4 @@ public class EnviaFila extends Thread{
         }
         
     }
-    
-    /*private final DatagramSocket socket;
-    private final BlockingQueue<DatagramPacket> fila;
-
-    public EnviaFila(DatagramSocket socket, BlockingQueue fila) {
-        this.socket = socket;
-        this.fila = fila;
-    }
-    
-    public void run() {
-        byte[] buffer = new byte[8192];
-        while(true) {
-            DatagramPacket pct = new DatagramPacket(buffer, buffer.length);
-            
-            try {
-                System.out.println("Aguardando requisição...");
-                socket.receive(pct);
-                System.out.println("Nova requisição de " + pct.getAddress());
-                fila.put(pct);
-            } catch (IOException ex) {
-                System.out.println("Ocorreu um erro ao receber o pacote!");
-            } catch (InterruptedException ex) {
-                System.out.println("Ocorreu uma interrupção enquanto a thread aguardava o atendimento da fila!");
-            }
-        }
-    }*/
-
 }
