b2427ea87f7eecf671e6c9ad9445310d9a39c6af
Julio Sieg <juliosieg@192.168.25.234>
2016-06-07 20:30:05 -0300

    Adicionada integração com Twitter

--- src/java/REST/RestWS.java
+++ src/java/REST/RestWS.java
@@ -26,6 +26,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 // an interface
 // The class registers its methods for the HTTP GET request using the @GET annotation. 
@@ -36,11 +41,23 @@ import javax.ws.rs.core.MediaType;
 @Path("/carro")
 public class RestWS {
 
+    public TwitterFactory carregaTwitter() {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("FFBLXqPDuWNWsMafY1u3e2VqC")
+                .setOAuthConsumerSecret("wO721YIf8ckSH6BjHrLg5t2IiVTE0QCkJYLfssmMF2OqrE5Ckj")
+                .setOAuthAccessToken("740316272257343488-GEd5ZsFGLOuEN1PHZZYBuHrZiXG4v7B")
+                .setOAuthAccessTokenSecret("hvLeKHes6xak5CUGyNdwJcF6hmurGqcp4IOYJPr2MxXeI");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+
+        return tf;
+    }
+
     @Path("/inserirCarro")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_HTML)
-    public String inserirCarro(String json) throws SQLException {
+    public String inserirCarro(String json) throws SQLException, TwitterException {
 
         Gson gson = new Gson();
 
@@ -59,6 +76,8 @@ public class RestWS {
         } else {
             try {
                 OperacoesBD.adicionaCarro(car);
+                Twitter twitter = carregaTwitter().getInstance();
+                twitter.updateStatus("Novo carro adicionado à base: " + car.getMarca() + " - " + car.getModelo());
                 return "OK";
             } catch (SQLException ex) {
                 return "Error";
@@ -85,10 +104,12 @@ public class RestWS {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String consultaPor(@PathParam("valor") String valor) throws SQLException {
-
         try {
             Carro car = OperacoesBD.consultaCarro(Integer.parseInt(valor));
+            
             return new Gson().toJson(car);
+            
+            
         } catch (Exception ex) {
 
             String retorno = "existe";
@@ -103,14 +124,15 @@ public class RestWS {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String alterarCarro(@PathParam("codigo") Integer codigo, String data) throws SQLException {
+    public String alterarCarro(@PathParam("codigo") Integer codigo, String data) throws SQLException, TwitterException {
 
         Carro car = new Gson().fromJson(data, Carro.class);
         car.setCodigo(codigo);
 
         try {
             OperacoesBD.alteraCarro(car);
-
+            Twitter twitter = carregaTwitter().getInstance();
+            twitter.updateStatus("Carro de código " + codigo + " alterado na base de dados");
             return "OK";
         } catch (SQLException ex) {
 
@@ -121,7 +143,7 @@ public class RestWS {
     @Path("/excluirCarro/{codigo}")
     @DELETE
     @Produces(MediaType.TEXT_PLAIN)
-    public String excluirCarro(@PathParam("codigo") Integer codigo) throws SQLException {
+    public String excluirCarro(@PathParam("codigo") Integer codigo) throws SQLException, TwitterException {
 
         Character flag = jaExiste(codigo);
         if (flag == 'n') {
@@ -133,6 +155,11 @@ public class RestWS {
 
             try {
                 OperacoesBD.deletaCarro(car);
+
+                Twitter twitter = carregaTwitter().getInstance();
+                
+                twitter.updateStatus("Carro de código "+car.getCodigo()+" excluído da base de dados");
+
                 return "OK";
             } catch (SQLException ex) {
                 return "Error";
